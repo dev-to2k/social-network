ec86731500e2ba843e8b0ce8a1230f8e236a2783
thanhtrung-to2k <trungnek175@gmail.com>
2024-03-15 06:10:12 +0700

    format code

--- client/src/components/home/comments/CommentCard.js
+++ client/src/components/home/comments/CommentCard.js
@@ -88,7 +88,7 @@ function CommentCard({ comment, post, commentId, children }) {
               <div className="mb-1 d-flex">
                 <div className="top p-3" style={onEdit ? widthFull : widthNormal}>
                   <Link to={`/profile/${comment.user._id}`} className="text-dark hover-underline">
-                    <h6>{comment.user.username}</h6>
+                    <h6>{comment.user.fullname}</h6>
                   </Link>
                   <div className="flex-fill text-wrap">
                     {onEdit ? (
@@ -97,7 +97,7 @@ function CommentCard({ comment, post, commentId, children }) {
                       <>
                         {comment.tag && comment.tag._id !== comment.user._id && (
                           <Link to={`profile/${comment.tag._id}`} className="me-1">
-                            @{comment.tag.username}
+                            @{comment.tag.fullname}
                           </Link>
                         )}
                         <span>
